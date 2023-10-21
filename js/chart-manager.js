45509e053da8e190d036eba5402959d4352f6a21
AliOding12 <aliz17043@gmail.com>
2023-10-21 13:00:00 +0500

    Optimize chart rendering performance

--- js/chart-manager.js
+++ js/chart-manager.js
@@ -1,3 +1,4 @@
 //yet to make this// Initialize chart-manager.js with chart setup
 // Add chart rendering logic to chart-manager.js
 // Add support for multiple chart types in chart-manager.js
+// Optimize chart rendering performance
