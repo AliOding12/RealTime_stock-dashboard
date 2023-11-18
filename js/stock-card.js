aebe3c79c2ae4900b31bdbe0a2eca4bcc32f6128
AliOding12 <aliz17043@gmail.com>
2023-11-18 12:00:00 +0500

    Add stock alerts in stock-card.js

--- js/stock-card.js
+++ js/stock-card.js
@@ -578,3 +578,4 @@ if (typeof module !== 'undefined' && module.exports) {
 // Add stock comparison feature to stock-card.js
 // Optimize stock card rendering
 // Add stock metrics display to stock-card.js
+// Add stock alerts in stock-card.js
